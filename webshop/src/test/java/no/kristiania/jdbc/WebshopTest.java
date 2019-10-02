5bcd07b2a1e4663ea6b9536257545ded3eae3998
Kamran Ali Iqbal <kamranali95@hotmail.com>
2019-10-02 11:22:46 +0200

    Add .travis.yml file

--- webshop/src/test/java/no/kristiania/jdbc/WebshopTest.java
+++ webshop/src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -1,4 +1,23 @@
 package no.kristiania.jdbc;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class WebshopTest {
+
+    @Test
+    void shouldRetrieveStoredProduct(){
+        ProductDao dao = new ProducutDao();
+        String productName = pickOne(new String[] {"Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+
+    private String pickOne(String[] strings) {
+        return strings[new Random().nextInt(strings.length)];
+
+    }
 }
